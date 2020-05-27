42a662f88a067ccb7e9cbffaa8e6548afc2bfdc8
Krishna Kompalli <kompalligk@gmail.com>
2020-05-27 10:03:19 +0530

    work done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'data';
+		Details = new FormGroup({
+		name: new FormControl('', Validators.required),
+		email: new FormControl('', Validators.required),
+		phoneNum: new FormControl('', Validators.required),
+		country: new FormControl('', Validators.required),
+		city: new FormControl('', Validators.required),
+		state: new FormControl('', Validators.required),
+		password: new FormControl('', Validators.required),
+		passwordConfirm: new FormControl('', Validators.required),
+		relation : new FormControl('', Validators.required),
+		favFood : new FormControl('', Validators.required),
+		favColor : new FormControl('', Validators.required)
+	  });
+	
+	constructor() {}
+	
+	selectedCountry: String = "--Select Country--";
+  
+	CountriesData: any[] = [
+    { 
+      name: 'India', 
+      states: [ {
+			name: 'Andhra Pradesh', 
+			cities: [{
+				name: "Visakhapatnam"
+			},
+		{
+			name:"Vijayawada"
+		},
+	{
+		name:"Guntur"
+	}]
+		},
+	{
+		name: "TamilNadu",
+		cities:[{
+			name: "Chennai"
+		},
+	{
+		name: "Coimbatore"
+	}]
+	} ] 
+  },
+
+		{ 
+			name: 'Norway', 
+			states: [ {
+				name: 'Northern State', 
+				cities: [
+					{
+						name:"Oslo"
+					},
+					{
+						name: "Trotsberg"
+					}
+				]
+			},
+		{
+			name:'Southern State',
+			cities: [{
+				name: "Valhalla"
+			},
+		{
+			name:"Aedda"
+		}
+	]
+		} ] 
+		
+		},
+	
+	];
+  
+	statesList:any[];
+
+	citiesList: any[];
+	
+	changeCountry(country) {
+		this.statesList = this.CountriesData.find(i => i.name == country).states;
+	}
+
+	changeState(state) {
+		this.citiesList = this.CountriesData.find(i => i.name == this.selectedCountry).states.find(p => p.name == state).cities;
+		console.log(this.citiesList)
+	}
+
+
+	onSubmit(){
+		!this.Details.valid ? alert('please enter required details') : alert('Thanks for the information')
+	}
 }
